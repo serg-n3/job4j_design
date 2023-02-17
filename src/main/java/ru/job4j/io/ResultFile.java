cd543ce4beb1f3396a152d634ea1f68cabd14914
serg-n3 <ktg80@yandex.ru>
2023-02-17 15:21:59 +0300

    FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            int array;
+            for (int row = 1; row <= 9; row++) {
+                for (int cell = 1; cell <= 9; cell++) {
+                    array = row  * cell;
+                    out.write((row + " x " + cell + " = " + array).getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
