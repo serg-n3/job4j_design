f4a47000607b3e81be2638998d05f1cd993efa5a
serg-n3 <ktg80@yandex.ru>
2023-02-20 19:49:14 +0300

    BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -19,13 +19,21 @@ public class LogFilter {
         }
         return list;
     }
-
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String lg : log) {
+                out.println(lg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("data/log.txt");
-        for (String lg : log) {
-            System.out.println(lg);
-        }
-
+        save(log, "data/404.txt");
     }
 }
