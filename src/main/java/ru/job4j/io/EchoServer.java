32d57ccdc525cc190b64f2d4b1841c84c5222196
serg-n3 <ktg80@yandex.ru>
2023-03-29 17:05:52 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,7 +5,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -14,11 +13,18 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String strok = in.readLine();
-                    if (strok.contains("Bye")) {
-                        server.close();
-                    }
+                    out.write("Hello, dear friend.\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        if (str.contains("msg=")) {
+                            if (str.contains("Exit")) {
+                                System.out.println("Server is closed!");
+                                server.close();
+                            } else if (str.contains("Hello")) {
+                                out.write("Hello".getBytes());
+                            } else {
+                                out.write("What".getBytes());
+                            }
+                        }
                         System.out.println(str);
                     }
                     out.flush();
