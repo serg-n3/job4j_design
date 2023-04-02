c0f13905039d270fed2e2b7c26dd4003ea4ad059
serg-n3 <ktg80@yandex.ru>
2023-04-02 19:37:51 +0300

    0.1. Связи между таблицами [#373615]

--- data/create.sql
+++ data/create.sql
@@ -2,19 +2,22 @@
 create table users(
 	id serial primary key,
     first_name text,
-	last_name text
+	last_name text,
+	id_role integer references role(id)
 );
 
 create table role(
 	id serial primary key,
-    name_role text,
-	id_user integer references users(id)
+    name_role text
+
 );
 
 create table item(
 	id serial primary key,
     name_item text,
-	id_user integer references users(id)
+	id_user integer references users(id),
+	id_category integer references category(id),
+	id_state integer references state(id)
 );
 
 create table comments (
@@ -31,14 +34,13 @@ create table attachs  (
 
 create table category   (
 	id serial primary key,
-    name_category   text,
-	id_item integer references item(id)
+    name_category   text
 );
 
 create table state    (
 	id serial primary key,
-    name_state   text,
-	id_item integer references item(id)
+    name_state   text
+
 );
 
 create table rules     (
--- data/insert.sql
+++ data/insert.sql
@@ -1,25 +1,25 @@
 
 
-insert into users(id, first_name, last_name)
+insert into users(id, first_name, last_name,id_role)
 values
-(1, 'Иван','Иванов'),
-(2, 'Петр','Петров'),
-(3, 'Сергей','Сидоров'),
-(4, 'Николай','Попов');
+(1, 'Иван','Иванов',1),
+(2, 'Петр','Петров',2),
+(3, 'Сергей','Сидоров',3),
+(4, 'Николай','Попов',4);
 
-insert into role(id, name_role, id_user)
+insert into role(id, name_role)
 values
-(1, 'Администратор',1),
-(2, 'Секретарь',2),
-(3, 'Пользователь',3),
-(4, 'Пользователь',4);
+(1, 'Администратор'),
+(2, 'Секретарь'),
+(3, 'Пользователь'),
+(4, 'Пользователь');
 
-insert into item(id, name_item, id_user)
+insert into item(id, name_item, id_user, id_category, id_state)
 values
-(1, 'заявка-1',1),
-(2, 'заявка-2',2),
-(3, 'заявка-3',2),
-(4, 'заявка-4',3);
+(1, 'заявка-1',1,1,2),
+(2, 'заявка-2',2,2,2 ),
+(3, 'заявка-3',2,3,1),
+(4, 'заявка-4',3,3,3);
 
 insert into comments(id, name_comment, id_user)
 values
@@ -35,19 +35,19 @@ values
 (3, 'файл-3',3),
 (4, 'файл-4',1);
 
-insert into category(id, name_category, id_user)
+insert into category(id, name_category)
 values
-(1, 'каттегория-1',1),
-(2, 'каттегория-2',2),
-(3, 'каттегория-3',3),
-(4, 'каттегория-4',1);
+(1, 'каттегория-1'),
+(2, 'каттегория-2'),
+(3, 'каттегория-3'),
+(4, 'каттегория-4');
 
-insert into state(id, name_state , id_user)
+insert into state(id, name_state)
 values
-(1, 'в пути',1),
-(2, 'доставлен',2),
-(3, 'на складе',3),
-(4, 'в пути',4);
+(1, 'в пути'),
+(2, 'доставлен'),
+(3, 'на складе');
+
 
 insert into rules(id, name_rules)
 values
