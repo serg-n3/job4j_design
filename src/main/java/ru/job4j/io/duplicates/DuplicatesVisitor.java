29b7741111e371eabbb3f64766baaa13e028e5e0
serg-n3 <ktg80@yandex.ru>
2023-04-02 17:42:38 +0300

    1. Схема прав пользователи и роли. [#1093]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -9,7 +9,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-       // if (file.toFile().getName().equals(FileProperty))
         System.out.println(file.toAbsolutePath());
         return super.visitFile(file, attrs);
     }
